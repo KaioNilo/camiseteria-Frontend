f4a1a537ecf5d258b856e5e6e23004663f70eba4
Kaio Nilo <kaionilofreitas@gmail.com>
2025-12-27 19:19:55 -0300

    trocando imagens locais por links cloudinary

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,46 +2,35 @@ import styles from './Banner.module.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectFade, Autoplay, Pagination } from 'swiper/modules';
 
-import Banner1 from '../../assets/Banner-1.png';
-import Banner2 from '../../assets/Banner-2.png';
-import Banner3 from '../../assets/Banner-3.png';
+const Banner1 = "https://res.cloudinary.com/dqk535bkf/image/upload/v1766873676/Banner_1_ty8hdv.png";
+const Banner2 = "https://res.cloudinary.com/dqk535bkf/image/upload/v1766873676/Banner_2_evmvrw.png";
+const Banner3 = "https://res.cloudinary.com/dqk535bkf/image/upload/v1766873676/Banner_3_kqsykk.png";
 
-
-function Banner({}) {
-
+function Banner() {
     const data = [
-        {id: '1', image: Banner1},
-        {id: '2', image: Banner2},
-        {id: '3', image: Banner3},
-    ]
+        { id: '1', image: Banner1 },
+        { id: '2', image: Banner2 },
+        { id: '3', image: Banner3 },
+    ];
 
     return (
         <section className={styles.banner}>
-
             <Swiper
                 modules={[EffectFade, Autoplay, Pagination]}
                 effect="fade"
                 slidesPerView={1}
-                spaceBetween={30}
                 loop={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false
-                }}
-                pagination={{
-                    clickable: true,
-                }}
+                autoplay={{ delay: 2500, disableOnInteraction: false }}
+                pagination={{ clickable: true }}
             >
-                {data.map(item => (
+                {data.map((item) => (
                     <SwiperSlide key={item.id}>
-                        <img src={item.image} alt="Banner"
-                        className='slide-item'/>
+                        <img src={item.image} alt="Banner" className={styles.slideImage} />
                     </SwiperSlide>
                 ))}
             </Swiper>
-
         </section>
-    )
+    );
 }
 
 export default Banner;
